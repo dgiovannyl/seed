17882a76601bda966fba336fdacc5e4923ff319b
t1jromero <jromero@todo1.net>
2018-11-27 16:21:00 -0500

    Add hello word

---
+++ src/app/my-component/my-component.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-component',
+  templateUrl: './my-component.component.html',
+  styleUrls: ['./my-component.component.css'],
+})
+export class MyComponentComponent implements OnInit {
+  name = 'Dayro';
+  count = 0;
+  imgSource =
+    'https://www.facevertizing.com/wp-content/uploads/2016/05/Nailed-It-Baby-Meme-06.jpg';
+  disabled = false;
+
+  disableAndCount() {
+    this.count = this.count + 1;
+    this.disabled = !this.disabled;
+  }
+
+  constructor() {}
+
+  ngOnInit() {}
+}
